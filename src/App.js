eb41538702c1a64c6d71fe3c118afc58ad39b389
ioay <oguzhan.ay@betek.com.tr>
2019-12-11 10:41:34 +0300

    axios-http-get api added.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      serverData : '',
+      persons: [],
+      userList: [],
+      user: ''
+    }
+
+    this.getData = this.getData.bind(this)
+    this.getUser = this.getUser.bind(this)
+    this.getUserList = this.getUserList.bind(this)
+    console.log(this.state.serverData)
+    console.log(this.state.user)
+  }
+
+  getData() {
+    axios.get('https://reqres.in/api/products/3')
+    .then(response => {
+        this.setState({ serverData: response.data.data.name })
+      })
+  }
+
+  //when page loads, get data from api
+  componentDidMount() {
+      axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+    }
+  // get an user data from api
+  getUser() {
+    axios.get('https://reqres.in/api/users/2')
+    .then(res => {
+      this.setState({ user : res.data.data.first_name })
+    })
+  }
+
+  getUserList() {
+    axios.get('https://reqres.in/api/users?page=2')
+      .then(res => {
+        this.setState({ userList: res.data.data })
+      })
+  }
+
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" onClick={this.getData}> Get Data From Api</button>
+        <p>Data From Server : {this.state.serverData} </p>
+        <button className="button" onClick={this.getUser}> Get User From Api</button>
+        <p> User Info From Server : {this.state.user} </p> 
+        <button className="button" onClick={this.getUserList}> Get Users From Api</button>
+        <p> The User List: </p>
+        <ul>
+          { this.state.userList.map(user => <li>{user.first_name} {user.last_name}</li>)} 
+        </ul>
+        <span> -  - - - - - - - - - -  -</span>
+        <p> The Person List: </p>
+        <ul>
+          { this.state.persons.map(person => <li>{person.name}</li>)}
+        </ul>
+      </div>
+    ) 
+  }
 }
 
-export default App;
+export default App
